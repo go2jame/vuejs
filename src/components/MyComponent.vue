876aea5be48b2bbed40c30e6c53ce75c638f0ffd
go2jame@gmail.com <go2jame@gmail.com>
2022-05-13 10:04:04 +0700

    Test Component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="hello">
+    <h1>TEST</h1>
+    <div>{{ getFullName() }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      firstName: "Thananan",
+      lastName: "Chaisiriphutthichok",
+    };
+  },
+  methods: {
+      getFullName() {
+          return this.firstName + " " + this.lastName;
+      }
+  }
+};
+</script>
+
+<style scoped>
+#hello{
+    color:red;
+}
+a {
+  color: #4a0148;
+}
+</style>
